0261563fbff1f8bab9d6930af1ea74d73d4829e2
hchiam <hchiam@gmail.com>
2017-04-24 21:51:10 -0400

    print works

--- interpreter.py
+++ interpreter.py
@@ -6,10 +6,30 @@ def open_file(file_name):
 
 def get_words(text):
     words = text.split(' ')
-    print(words)
+    print_state = False
+    print_string = ''
+    for word in words:
+        if word == 'print':
+            print_state = True
+        elif print_state == True:
+            if word != 'please':
+                if print_string != '':
+                    print_string += ' ' + word
+                else:
+                    print_string += word
+            elif word == 'please':
+                print(print_string + '   "please"')
+                # reset print variables
+                print_state = False
+                print_string = ''
+        elif word == 'please':
+            # reset all variables?
+            print_state = False
+            print_string = ''
 
 def interpret():
     text = open_file(argv[1])
+    text = text.lower()
     get_words(text)
 
 interpret()
