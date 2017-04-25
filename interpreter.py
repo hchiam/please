ea5e0f18a1f44d95031792fc5f1960fa2fba6058
hchiam <hchiam@gmail.com>
2017-04-24 23:42:54 -0400

    separate get sents and get words

--- interpreter.py
+++ interpreter.py
@@ -6,22 +6,31 @@ from sys import *
 def interpret():
     text = open_file(argv[1])
     text = text.lower()
-    get_words(text)
+    sentences = get_sentences(text)
+    print(sentences)
+    words = get_words_grouped_by_sentence(sentences)
+    print(words)
 
 def open_file(file_name):
     text = open(file_name, 'r').read()
     return text
 
-def get_words(text):
+def get_sentences(text):
     sentences = text.split('please')
-    for sentence in sentences:
-        words = sentence.split(' ')
-        # print(words)
-        words_count = len(words)
-        for i, word in enumerate(words):
-            words_left = words_count - i
-            sentence_data = sentence_info(word, words_left)
-            check_print(sentence_data)
+    return sentences
+
+def get_words_grouped_by_sentence(sentences):
+    word_groups = [] # instead of "= sentences", which would pass by reference
+    for i in range(len(sentences)):
+        words = list(sentences[i].strip().split(' '))
+        word_groups.append(words)
+    return word_groups
+        # # print(words)
+        # words_count = len(words)
+        # for i, word in enumerate(words):
+        #     words_left = words_count - i
+        #     sentence_data = sentence_info(word, words_left)
+        #     check_print(sentence_data)
 
 def check_print(sentence_data):
     global print_state
