2db675afd6e6491da03d9dc417d9b739baece4b0
hchiam <hchiam@gmail.com>
2017-05-18 14:19:43 -0400

    lists can handle negative numbers now (#55)

--- code.py
+++ code.py
@@ -4,10 +4,10 @@ banana = None
 apple = 1
 print("variable apple")
 banana = 300
-coconut = some words
-durian = [8, 9, 10, 11, 12]
-crazylist = [1, 2, 'tree bark']
-result = 1+2
+coconut = 'some words'
+durian = [ 8 , 9 , 10 , 11 , 12 ]
+crazy_list = [ 1 , 2 , 'tree bark' ]
+result = '1+2'
 print("variable result")
 if True:
 	print("this is a 1 line if statement")
@@ -23,7 +23,7 @@ if 1==2:
 	print("it should ! print this end if")
 
 print("and this line")
-circle = [-1, 0, 1, 2, 3]
+circle = [ -1 , 0 , 1 , 2 , 3 ]
 for index in circle:
 	print("variable index")
 
@@ -31,19 +31,19 @@ for index in circle:
 def test(item):
 	print("variable item")
 
-other = it works
+other = 'it works'
 test(other)
 import alternate
 from library import test
 test.test_function()
 test.test_function()
-crazylist = [1, 2, 'tree bark']
-for item in crazy list:
+crazy_list = [ 1 , 2 , 'tree bark' ]
+for item in crazy_list:
 	print("index variable item of crazy list")
 
-import numpy as numbpie
-array = [2, 3, 4]
+import numpy as numb_pie
+array = [ 2 , 3 , 4 ]
 print("array is variable array")
 print("use array of numb pie on variable array")
-output = use array of numb pie on array
+output = 'use array of numb pie on variable array'
 print("output of array of numb pie is variable output")
