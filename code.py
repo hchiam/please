749ee817bd5ca41263345a64e52dfa2fca3b00dd
hchiam <hchiam@gmail.com>
2017-05-19 17:34:38 -0400

    both interpreter.py and transformer.py work on this

--- code.py
+++ code.py
@@ -1,14 +1,49 @@
-def fibonacci(number):
-	if number <2:
-		return number
-	
-	apple = fibonacci(number -1)
-	banana = fibonacci(number -2)
-	coconut = apple + banana
-	return coconut
+print("this string of words")
+apple = None
+banana = None
+apple = 1
+print(str(apple))
+banana = 300
+coconut = 'some words'
+durian = [ 8 , 9 , 10 , 11 , 12 ]
+crazy_list = [ 1 , 2 , 'tree bark' ]
+result = 1+2
+print(str(result))
+if True:
+	print("this is a 1 line if statement")
 
-input = [ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 ]
-for number in input:
-	output = fibonacci(number)
-	print(str(output))
+if 1==1:
+	print("this should print")
 
+print("there should be nothing between this line")
+if 1==2:
+	print("it should not print this")
+
+if 1==2:
+	print("it should ! print this end if")
+
+print("and this line")
+circle = [ -1 , 0 , 1 , 2 , 3 ]
+for index in circle:
+	print(str(index))
+
+# this is a comment
+def test(item):
+	print(str(item))
+
+other = 'it works'
+test(other)
+import alternate
+from library import test
+test.test_function()
+test.test_function()
+crazy_list = [ 1 , 2 , 'tree bark' ]
+for item in crazy_list:
+	print(str(item))
+
+import numpy as numb_pie
+array = [ 2 , 3 , 4 ]
+print("array is " + str(array))
+print("use array of numb pie on " + str(array))
+output = numb_pie.array(array)
+print("output of array of numb pie is " + str(output))
